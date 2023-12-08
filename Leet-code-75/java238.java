b1467d19084ace0b8aa1251868bc6fdb1652cd3d
ohju96 <ohju96@naver.com>
2023-12-08 23:26:52 +0900

    [링크](https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=leetcode-75)

---
+++ Leet-code-75/java238.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class java238 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+
+        int[] nums2 = {-1, 1, 0, -3, 3};
+        System.out.println(Arrays.toString(productExceptSelf(nums2)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans, 1);
+        int curr = 1;
+        for(int i = 0; i < n; i++) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        return ans;
+    }
+}
